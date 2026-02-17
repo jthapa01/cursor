b053ba6fe472b67741db0583f851613d104bc41d
jeevan thapa <jeevanthapa@microsoft.com>
2026-02-17 17:02:55 -0500

    03: Database Setup

---
+++ src/features/auth/components/unauthenticated-view.tsx
@@ -0,0 +1,29 @@
+import { ShieldAlertIcon } from "lucide-react";
+import { Item, ItemActions, ItemContent, ItemDescription, ItemTitle, ItemMedia } from "@/components/ui/item";
+import { SignInButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+
+export const UnauthenticatedView = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-background">
+      <div className="w-full max-w-lg bg-muted">
+        <Item variant="outline">
+          <ItemMedia variant="icon">
+            <ShieldAlertIcon />
+          </ItemMedia>
+          <ItemContent>
+            <ItemTitle>Unauthorized Access</ItemTitle>
+            <ItemDescription>
+              You must be signed in to view this page.
+            </ItemDescription>
+          </ItemContent>
+          <ItemActions>
+            <SignInButton>
+              <Button variant="outline" size="sm">Sign In</Button>
+            </SignInButton>
+          </ItemActions>
+        </Item>
+      </div>
+    </div>
+  );
+};
