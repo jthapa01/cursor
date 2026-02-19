d61d08d0e0a3b43c85278edae3f2476356e74d3e
jeevan thapa <jeevanthapa@microsoft.com>
2026-02-19 10:18:48 -0500

    04: Background Jobs

---
+++ src/app/api/demo/blocking/route.ts
@@ -0,0 +1,11 @@
+// POST localhost:3000/api/demo/blocking
+import { generateText } from "ai";
+import { anthropic } from "@ai-sdk/anthropic";
+
+export async function POST() {
+  const response = await generateText({
+    model: anthropic("claude-3-haiku-20240307"),
+    prompt: "Write a vegetarian momo recipe.",
+  });
+  return Response.json({ status: "completed", response });
+};
---
+++ src/inngest/client.ts
@@ -0,0 +1,6 @@
+import { Inngest } from "inngest";
+
+// Create an inngest client to send and receive data from your functions.
+export const inngest = new Inngest({
+  id: "cursor"
+});
